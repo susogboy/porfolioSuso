381d64c69f5bd562d493064ef89efeef4f0c8788
Jesus Fernandez <jesus.fersan2003@gmail.com>
2024-07-14 16:18:35 +0200

    fix(tokens)

--- astro.config.ts
+++ astro.config.ts
@@ -1,16 +1,15 @@
-import { defineConfig } from 'astro/config'
-import react from '@astrojs/react'
-import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  site: process.env.CI
-    ? 'https://astro-shadcn-ui-template.vercel.app'
-    : 'http://localhost:4321',
-  integrations: [
-    react(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
-  ],
-})
+  site: process.env.CI ? 'https://astro-shadcn-ui-template.vercel.app' : 'http://localhost:4321',
+  integrations: [react(), tailwind({
+    applyBaseStyles: false
+  })],
+  output: "server",
+  adapter: vercel()
+});
--- src/lib/contentful.ts
+++ src/lib/contentful.ts
@@ -1,4 +1,4 @@
-import contentful from 'contentful'
+import contentful from 'contentful';
 
 export const contentfulClient = contentful.createClient({
   space: import.meta.env.CONTENTFUL_SPACE_ID,
@@ -6,4 +6,4 @@ export const contentfulClient = contentful.createClient({
     ? import.meta.env.CONTENTFUL_PREVIEW_TOKEN
     : import.meta.env.CONTENTFUL_DELIVERY_TOKEN,
   host: import.meta.env.DEV ? 'preview.contentful.com' : 'cdn.contentful.com',
-})
+});
